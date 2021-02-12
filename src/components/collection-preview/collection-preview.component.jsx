cf8bd5e37aa848114c7b9b606a77f9eb398c1ad9
Kelvinbdavis <37672442+Kelvinbdavis@users.noreply.github.com>
2021-02-11 18:57:50 -0700

    added collection item components and styling for collection preview of shop pages

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
-import './collection-preview.scss'
+import './collection-preview.styles.scss'
+
+import CollectionItem from '../collection-item/collection-item.component.jsx'
 
 const CollectionPreview = ({ title, items }) => (
-    <div>
+    <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className="preview">
             {items
                 .filter((items, idx) => idx < 4)
-                .map((item) => (
-                    <div key={item.id}>{item.name}</div>
+                .map(({ id, ...itemProps }) => (
+                    <CollectionItem key={id} {...itemProps} />
                 ))}
         </div>
     </div>
