779845e37ad05663e7d5472ad780bfc067d84e4a
Kelvinbdavis <37672442+Kelvinbdavis@users.noreply.github.com>
2021-02-24 21:57:53 -0700

    updated code to redirect if user signed in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux'
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
@@ -11,8 +11,8 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
 
 const App = () => {
-  //sd
   const dispatch = useDispatch()
+  const currentUser = useSelector((state) => state.user.currentUser)
   useEffect(() => {
     const unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -33,9 +33,10 @@ const App = () => {
 
     })
     return () => {
+
       unsubscribeFromAuth();
     }
-  }, [])
+  }, [dispatch])
 
 
   return (
@@ -44,7 +45,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
+        <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
       </Switch>
     </div>
   );
