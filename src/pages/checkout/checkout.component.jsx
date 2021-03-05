eab785a6d329aabca9435b4fa9216ae1427d2c41
Kelvinbdavis <37672442+Kelvinbdavis@users.noreply.github.com>
2021-03-05 11:42:06 -0700

    added checkout page, checkout component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
+import './checkout.styles.scss';
 
 import { useSelector } from 'react-redux'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector'
-
-import './checkout.styles.scss';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 const CheckoutPage = () => {
     const cartItems = useSelector(state => selectCartItems(state))
     const cartTotal = useSelector(state => selectCartTotal(state))
     return (
-        <div className="checkout-page">
-            <div className="checkout-header">
-                <div className="header-blocks">
+        <div className='checkout-page'>
+            <div className='checkout-header'>
+                <div className='header-block'>
                     <span>Product</span>
                 </div>
-                <div className="header-blocks">
+                <div className='header-block'>
                     <span>Description</span>
                 </div>
-                <div className="header-blocks">
-                    <span>Quanity</span>
+                <div className='header-block'>
+                    <span>Quantity</span>
                 </div>
-                <div className="header-blocks">
+                <div className='header-block'>
+                    <span>Price</span>
+                </div>
+                <div className='header-block'>
                     <span>Remove</span>
                 </div>
             </div>
-            {cartItems.map(cartItem => cartItem.name)}
-            <div className="total">
-                <span>TOTAL: ${cartTotal}</span>
-            </div>
+            {cartItems.map(cartItem => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))}
+            <div className='total'>TOTAL: ${cartTotal}</div>
         </div>
     )
 }
